80a4f33422ce50c8d13ef45a4ac001e2c38a9f51
zuosi <zuosi@anji-plus.com>
2018-10-18 15:32:55 +0800

    安防

---
+++ wx/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <!--<router-view v-wechat-title='$route.meta.title'></router-view>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<style lang="less">
+@import "./common/css/common.css";
+</style>
