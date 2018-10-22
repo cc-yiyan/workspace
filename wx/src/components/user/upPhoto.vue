52ff108186d9892e5916238e22a8d7358e567f4b
zuosi <zuosi@anji-plus.com>
2018-10-22 15:04:56 +0800

    上传头像页面

--- wx/src/components/user/upPhoto.vue
+++ wx/src/components/user/upPhoto.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
-photo
+        qq{{this.$route.query.loginName}}
     </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      viewH: ""
+    };
+  },
+  mounted() {
+    this.viewH = window.innerHeight;
+    this.loginName = this.$route.query.loginName;
+    this.company = this.$route.query.company;
+    this.department = this.$route.query.department;
+    this.mailbox = this.$route.query.mailbox;
+  }
+};
 </script>
 
 <style scoped>
