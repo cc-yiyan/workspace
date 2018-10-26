0ce7084d49831611d173a0583b722d1985e719ab
zuosi <zuosi@anji-plus.com>
2018-10-26 14:18:58 +0800

    个人中心

--- wx/src/components/user/getUserList.vue
+++ wx/src/components/user/getUserList.vue
@@ -1,35 +1,36 @@
 <template>
   <div style="font-size: .32rem" class="login-bg" :style="{height: viewH+'px'}" >
-    <div class="head">头像
-        <span>姓名</span>
+    <div class="head">
+      <img :src="item.headimgurl" class="user-img">
+        <span>{{item.nickName}}</span>
     </div>
     <div class="middle" >
        <div class="input-bar">
-         绑定号:{{item.no}}
+         绑定号:{{item.bindNum}}
       </div>
        <div class="input-bar">
         公司:{{item.company}}
       </div>
        <div class="input-bar">
-        部门:{{item.depart}}
+        部门:{{item.department}}
       </div>
       <div class="input-bar">
-        邮箱:{{item.mail}}
+        邮箱:{{item.mailbox}}
       </div>
       <div class="input-bar">
-        手机号:{{item.tel}}
+        手机号:{{item.phone}}
       </div>
       <div class="input-bar">
-        已邀请:{{item.invite}}
+        已邀请:{{item.invitedNum}}
       </div>
       <div class="input-bar">
-        积分:{{item.count}}
+        积分:{{item.score}}
       </div>
       <div class="input-bar">
-        绑定状态:{{item.isBind}}
+        绑定状态:{{item.auditState}}
       </div>
     </div>
-    <div class="bottom" >返回</div>
+    <div class="bottom" @click="close">返回</div>
     
   </div>
 </template>
@@ -39,21 +40,69 @@ export default {
   data() {
     return {
       item: {
-        no: 111,
-        company: "安吉",
-        depart: "",
-        mail: "",
-        tel: "",
-        invite: "",
-        count: "",
-        isBind: ""
+        // nickName: "", //姓名
+        // openid: "", //绑定号
+        // company: "", //公司
+        // department: "", //bumen
+        // mailbox: "", //邮箱
+        // phone: "", //手机号
+        // invitedNum: "", //已邀请
+        // score: "", //积分
+        // auditState: "", //绑定状态,
+        // headimgurl: "" //用户头像
       },
-
       viewH: "" //动态获取视窗大小
     };
   },
   mounted() {
+    this.loadData();
     this.viewH = window.innerHeight;
+  },
+  methods: {
+    loadData() {
+      let self = this;
+      self.$api.post(
+        "register/queryUserInfo?userId=15",
+        {},
+        r => {
+          console.log(r, 111);
+          this.item = r.data;
+          // self.item = {
+          //   nickName: r.repData.nickName,
+          //   openid: r.repData.openid,
+          //   company: r.repData.company,
+          //   department: r.repData.department,
+          //   mailbox: r.repData.mailbox,
+          //   phone: r.repData.phone,
+          //   invitedNum: r.repData.invitedNum,
+          //   score: r.repData.score,
+          //   auditState: r.repData.auditState,
+          //   headimgurl: r.repData.headimgurl
+          // };
+
+          // if (r.repData.imgUrl != "") {
+          //   self.contact.httpPicture = r.repData.imgUrl.split(",");
+          // }
+        },
+        e => {
+          // if (e.repCode == "0104") {
+          //   sessionStorage.clear();
+          //   self.$api.getId(this.$route);
+          // } else {
+          self.$vux.toast.show({
+            text: e.repMsg,
+            time: "2000",
+            type: "text",
+            position: "middle"
+          });
+          // }
+        }
+      );
+    },
+    close() {
+      WeixinJSBridge.call("closeWindow");
+      // this.$route.go(-1);
+    }
   }
 };
 </script>
@@ -112,5 +161,12 @@ export default {
   height: 100%;
   line-height: inherit;
   color: #999;
+}
+.user-img {
+  width: 3.5em;
+  height: 3.5em;
+  border-radius: 100%;
+  overflow: hidden;
+  margin: 0 auto;
 }
 </style>
